2a23e743dbdd9e3e866b3206f3809ba9e8cc468d
Adegoke Obasa <adegokeobasa@gmail.com>
2018-02-03 19:09:43 +0100

    update: solution to pointers quiz

--- lesson_5/pointers.cpp
+++ lesson_5/pointers.cpp
@@ -1,6 +1,6 @@
 /*For this program print for each variable
-**print the value of the variable, 
-**then print the address where it is stored. 
+**print the value of the variable,
+**then print the address where it is stored.
 */
 #include<iostream>
 #include<string>
@@ -12,33 +12,35 @@ int main()
     double givenDouble ;
     std::string givenString;
     char givenChar;
-    
+
     std::cin >> givenInt;
     int * intPointer = &givenInt;
     std::cin >> givenFloat;
+    std::cin.ignore();
     float * floatPointer = &givenFloat;
     std::cin >> givenDouble;
     double * doublePointer = &givenDouble;
+    std::cin.ignore();
     std::cin >> givenChar;
     char * charPointer = &givenChar;
-    std::cin >> givenString;
+    std::cin.ignore();
+    std::getline(std::cin, givenString);
     std::string * stringPointer = &givenString;
-    
+
     std::cout << *intPointer << "\n";
     std::cout << intPointer << "\n";
-    
+
     std::cout << *floatPointer << "\n";
     std::cout << floatPointer << "\n";
-    
+
     std::cout << *doublePointer << "\n";
     std::cout << doublePointer << "\n";
-    
+
     std::cout << *stringPointer << "\n";
     std::cout << stringPointer << "\n";
-    
-    std::cout << *charPointer << "\n";
-    std::cout << charPointer << "\n";
-    
+
+    std::cout <<  (char) *charPointer << "\n";
+    std::cout << (void *) charPointer << "\n";
+
     return 0;
 }
-
